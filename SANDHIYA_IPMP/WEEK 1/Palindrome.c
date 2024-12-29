d922fbc205194cdf811ac6cff33075e226708f02
Sandhiya <151381016+Sjv7@users.noreply.github.com>
2024-12-29 14:28:59 +0530

    Update Palindrome.c

--- SANDHIYA_IPMP/WEEK 1/Palindrome.c
+++ SANDHIYA_IPMP/WEEK 1/Palindrome.c
@@ -2,20 +2,13 @@
 #include<stdbool.h>
 bool isPalindrome(long int x) {
     long int n=x,r=0,rev=0;
-    long prev=0;
     if(x<0) return false;
     while(n!=0)
     {
         r=n%10;
-         printf("R %d ",r);
         rev=r+(10*rev);
-        // if((rev-r)/10 != prev)
-        // return false;
-        // prev=rev;
-        printf(" %d \n",rev);
         n=n/10;
     }
-    printf("REV: %d\n",rev);
     if (rev==x)
     {
         return true;
@@ -28,6 +21,7 @@ int main()
     num=1234554321;
     bool a;
     a=isPalindrome(num);
-    printf(" %d ",a);
+    if(a) printf("True");
+    else printf("False");
     return 0;
 }
