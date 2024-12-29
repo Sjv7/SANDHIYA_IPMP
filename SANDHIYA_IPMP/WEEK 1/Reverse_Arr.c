0ff878c76e3ced2761cc34d83f9d4007c2cc2df0
Sandhiya <151381016+Sjv7@users.noreply.github.com>
2024-12-29 14:30:48 +0530

    Update Reverse_Arr.c

--- SANDHIYA_IPMP/WEEK 1/Reverse_Arr.c
+++ SANDHIYA_IPMP/WEEK 1/Reverse_Arr.c
@@ -7,24 +7,24 @@ int main()
     int arr[n];
     char str[n];
 
-    // for(i=0;i<n;i++) scanf("%d",&arr[i]);
+    for(i=0;i<n;i++) scanf("%d",&arr[i]);
     // for(i=0;i<n;i++) scanf("%s",&str[i]); str[i]='\0';
-    scanf("%s",str);
+    // scanf("%s",str);
 
     // for(i=0;i<n;i++) printf("%d ",arr[i]);
     printf("\n");
     // for(i=0;i<n || str[i]!='\0';i++) printf("%c ",str[i]);
-    printf("%s ",str);
+    // printf("%s ",str);
 
-    // for(i=0;i<num;i++,num--)
-    // {
-    //     int temp=arr[i];
-    //     arr[i]=arr[num-1];
-    //     arr[num-1]=temp;
-    // }
+    for(i=0;i<num;i++,num--)
+    {
+        int temp=arr[i];
+        arr[i]=arr[num-1];
+        arr[num-1]=temp;
+    }
 
-    // for(i=0;i<n;i++) printf("%d ",arr[i]);
-    // printf("\n");
+    for(i=0;i<n;i++) printf("%d ",arr[i]);
+    printf("\n");
 
     return 0;
 }
