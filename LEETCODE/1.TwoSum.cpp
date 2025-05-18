b46785bea932617cb29709fb0046c74a91d069f8
Sandhiya <sjv72006@gmail.com>
2025-05-19 01:08:55 +0530

    Commit Loading.

--- LEETCODE/1.TwoSum.cpp
+++ LEETCODE/1.TwoSum.cpp
@@ -8,12 +8,26 @@ class Solution {
             {
                 for(int j = i + 1; j < n; j++)
                 {
+                    cout << "* ";
                     if(a[i] + a[j] == target)
                     {
                         return {i,j};
                     }
                 }
+                cout << endl;
             }
+            cout << "not possible.";
             return {};
         }
     };
+
+    int main()
+    {
+        Solution s;
+        vector<int> a = {1,2,3,4};
+        int t;
+        cin >> t;
+        vector<int> res = s.twoSum(a,t);
+        cout << res[0] << " " << res[1];
+        return 0;
+    }
